6418a46957a80893773e9662820e589f7141999f
Anton Imanov <antony.imanov@gmail.com>
2020-11-15 13:45:44 +0200

    Added Standalone structure

---
+++ Standalone_structure/frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes/Routes";
+
+import Header from "./components/ui/Header";
+import Footer from "./components/ui/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes />
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
