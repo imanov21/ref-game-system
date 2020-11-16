b247040b0bdb44b702647ff569bd0a1e0673a45e
Anton Imanov <antony.imanov@gmail.com>
2020-11-16 19:14:23 +0200

    Updated paths

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    User = require("models/user");
+    User = require("/models/user");
 
 //requiring routes
 
